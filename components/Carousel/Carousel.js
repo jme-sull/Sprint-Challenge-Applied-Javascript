624ae1fec51c91af3a33be3a718914162317c0da
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-03-13 12:27:30 -0700

    stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,65 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function createCarousel(){ 
+  const carousel = document.createElement('div');
+  const carouselButtonLeft = document.createElement('div');
+  const carouselImageOne = document.createElement('img');
+  const carouselImageTwo = document.createElement('img');
+  const carouselImageThree = document.createElement('img');
+  const carouselImageFour = document.createElement('img');
+  const carouselButtonRight = document.createElement('div');
+
+  carousel.classList.add('carousel');
+  carouselButtonLeft.classList.add('left-button');
+  carouselButtonRight.classList.add('right-button');
+
+  carouselImageOne.style.display = 'block';
+
+  let i = 0;
+  let images = [];
+
+   //image list 
+   images[0] = './assets/carousel/mountains.jpeg';
+   images[1] = './assets/carousel/computer.jpeg';
+   images[2] = './assets/carousel/trees.jpeg';
+   images[3] = './assets/carousel/turntable.jpeg';
+
+  carouselImageOne.setAttribute('src', images[0]);
+  
+
+  carousel.append(carouselButtonLeft);
+  carousel.append(carouselImageOne);
+  carousel.append(carouselImageTwo);
+  carousel.append(carouselImageThree);
+  carousel.append(carouselImageFour);
+  carousel.append(carouselButtonRight);
+
+  carouselButtonRight.addEventListener('click', () => {
+    carouselImageOne.src = images[i];
+    if(i < images.length - 1){
+      i++
+    } else {
+      i = 0; 
+    }
+
+  })
+
+  carouselButtonLeft.addEventListener('click', () => {
+    carouselImageOne.src = images[i];
+    if(i < images.length - 1){
+      i = i - 1;
+    } else {
+      i = 0; 
+    }
+
+  })
+ 
+  return carousel;
+
+}
+
+
+document.querySelector('.carousel-container').append(createCarousel());
+
